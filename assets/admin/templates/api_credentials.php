c681c314172960f07108244fd0fa561bae170140
Jesse Wozniak <wozniak.jesse@gmail.com>
2017-02-07 23:46:38 -0500

    setup admin template to display api credentials form

--- assets/admin/templates/api_credentials.php
+++ assets/admin/templates/api_credentials.php
@@ -2,4 +2,14 @@
     
     <h2><?php echo SBWP_PLUGIN_NAME; ?></h2>
     
+    <form method="POST" action="options.php">
+        
+        <?php settings_fields('sbwp-api-credentials'); ?>
+        
+        <?php do_settings_sections('sbwp-api-credentials'); ?>
+        
+        <?php submit_button(); ?>
+        
+    </form>
+    
 </div>
