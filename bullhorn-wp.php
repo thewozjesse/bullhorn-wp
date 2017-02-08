f676ac66edf957ff004468297d425bdf57676092
Jesse Wozniak <wozniak.jesse@gmail.com>
2017-02-07 22:22:31 -0500

    initialize plugin

---
+++ bullhorn-wp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Simple Bullhorn WP
+ * Plugin URI: https://github.com/thewozjesse/bullhorn-wp
+ * Description: This plugin grabs job postings from the Bullhorn Staffing API
+ * Version: 1.0.0
+ * Author: Jesse Wozniak
+ * Author URI: http://thewozjesse.com
+ */
+ 
+ ?>
